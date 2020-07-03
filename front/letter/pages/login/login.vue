9958d44542f47840e9388850360f011f4ac93ca4
MaplePudding <fengtangbuding@gmail.com>
2020-07-03 14:45:38 +0800

    Completed most of the page construction

---
+++ front/letter/pages/login/login.vue
@@ -0,0 +1,139 @@
+<template>
+	<view>
+		<view id="top">
+
+		</view>
+		<view class="login">
+			Login
+		</view>
+		<view class="form">
+			<view class="name">
+				<input placeholder="name" v-model="name"/>
+				<view class="tips" v-show="checkName">please enter name</view>
+			</view>
+			<view class="password">
+				<input placeholder="password" type="password" v-model="password"/>
+				<view class="tips" v-show="checkPwd">please enter password</view>
+				<view class="tips" v-if="false">password or name error</view>
+			</view>
+			<button v-bind:disabled="checkButton">login</button>
+			<view class="signup" v-on:click="toSignUp">
+				<image src="../../static/login/sign-up.png"></image>sign-up
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name: '',
+				password: ''
+			}
+		},
+		methods: {
+			
+			/**
+			 * redirect to signup.vue
+			 * */
+			 
+			toSignUp: function() {
+				uni.navigateTo({
+					url: '../signup/signup',
+				})
+			}
+		},
+		computed:{
+			
+			/**
+			 * Checks if name is ''
+			 * */
+			 
+			checkName: function(){
+				if(this.name == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * Checks if password is ''
+			 * */
+			 
+			checkPwd: function(){
+				if(this.password == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * button -> disabled
+			 * */
+			
+			checkButton: function(){
+				if(this.name == '' || this.password == ''){
+					return true;
+				}
+				return false;
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("../../static/common/css/common.css");
+
+	.login {
+		text-align: center;
+		margin-top: 80px;
+		font-size: 2em;
+	}
+
+
+
+	button {
+		margin-top: 40px;
+	}
+	
+	.name{
+		height: 95px;
+	}
+
+	.form {
+		width: 90%;
+		display: flex;
+		flex-direction: column;
+		margin-top: 30px;
+		justify-content: center;
+		align-items: center;
+	}
+	
+	.password{
+		height: 60px;
+	}
+
+	.form image {
+		display: inline-flex;
+		width: 20px;
+		height: 20px;
+	}
+
+
+	.signup {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		margin-top: 15px;
+		color: #8F8F94;
+		padding-bottom: 4px;
+		border-bottom: 1px solid #8F8F94;
+	}
+
+	.signup image {
+		margin-right: 5px;
+		align-self: baseline;
+	}
+</style>
---
+++ front/letter/pages/signup/signup.vue
@@ -0,0 +1,141 @@
+<template>
+	<view>
+		<view id="top"></view>
+		<image src="../../static/signup/back.png" v-on:click="back"></image>
+		<view id="signup">Sign Up</view>
+		<view class="form">
+			<view class="name">
+				<input placeholder="name" v-model="name"/>
+				<view class="tips" v-if="checkName">Username can not be empty</view>
+				<view class="tips" v-if="false">Username already exists</view>
+			</view>
+			<view class="email">
+				<input placeholder="email" v-model="email"/>
+				<view class="tips" v-if="checkEmail">Email can not be empty</view>
+				<view class="tips" v-if="vertifyEmail">Email format error</view>
+			</view>
+			<view class="password">
+				<input type="password" placeholder="password" v-model="password"/>
+				<view class="tips" v-if="checkPwd">password can not be empty</view>
+			</view>
+			<button v-bind:disabled="checkButton || flag">Sign up</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name: '',
+				password: '',
+				email: '',
+				flag: true
+			}
+		},
+		methods: {
+			
+			/**
+			 * Return to the login.vue
+			 * */
+			 
+			back: function(){
+				uni.navigateBack({
+					
+				})
+			}
+		},
+		computed:{
+			
+			/**
+			 * Checks if name is ''
+			 * */
+			 
+			checkName: function(){
+				if(this.name == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * Checks if password is ''
+			 * */
+			 
+			checkPwd: function(){
+				if(this.password == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * button -> disabled
+			 * */
+			
+			checkButton: function(){
+				if(this.name == '' || this.password == '' || this.email == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * Checks if password is ''
+			 * */
+			 
+			checkEmail: function(){
+				if(this.email == ''){
+					return true;
+				}
+				return false;
+			},
+			
+			/**
+			 * Vertify email
+			 * */
+			 
+			 vertifyEmail: function(){
+				 var regex = /^([A-Za-z0-9_\-\.\u4e00-\u9fa5])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,8})$/g;
+				 return this.flag = !this.email.match(regex);
+			 }
+		}
+	}
+</script>
+
+<style>
+	@import url("../../static/common/css/common.css");
+	image{
+		width: 25px;
+		height: 25px;
+		margin-top: 10px;
+		margin-left: 5px;
+	}
+	
+	#signup{
+		margin-top: 45px;
+		font-size: 2em;
+		text-align: center;
+	}
+	
+	.form{
+		width: 90%;
+		margin: 20px auto;
+		display: flex;
+		flex-direction: column;
+		align-items: baseline;
+		justify-content: center;
+	}
+	
+	.name{
+		height: 85px;
+	}
+	
+	.password{
+		height: 85px;
+	}
+	
+	.email{
+		height: 85px;
+	}
+</style>
