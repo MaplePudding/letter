1bc5f48a786f7d48970cf29b728de117a3e6e942
MaplePudding <fengtangbuding@gmail.com>
2020-07-07 11:34:09 +0800

    Completed the app.js construction

---
+++ server/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = require('./router.js');
+var bodyParser = require('body-parser');
+var app = express();
+
+/**
+ * Parse post request
+ */
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(router);
+app.listen('3000', function(){
+    console.log('listen successfun');
+})
---
+++ server/router.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var router = express.Router();
+
+router.get('/', function(){
+
+})
+
+router.get('/', function(){
+
+})
+
+router.get('/', function(){
+
+})
+
+router.get('/', function(){
+
+})
+
+router.post('/', function(){
+
+})
+
+
+module.exports = router;
